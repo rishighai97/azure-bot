a6e503d763f6ba6ca05109f7968ddcbe2b1a0927
rishighai97 <rishighai97@gmail.com>
2021-08-31 11:25:53 +0530

    Adding image processing to bot

--- app.py
+++ app.py
@@ -1,71 +1,102 @@
-from flask import Flask, request, Response
-from botbuilder.core import BotFrameworkAdapter, BotFrameworkAdapterSettings, TurnContext, ConversationState, \
-    MemoryStorage
-from botbuilder.schema import Activity
-import asyncio
-from qna_bot import QnaBot
-import requests
-import logging
-from properties import *
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 
-app = Flask(__name__)
-log = logging.getLogger('werkzeug')
-log.disabled = True
-loop = asyncio.get_event_loop()
+import sys
+import traceback
+from datetime import datetime
+from http import HTTPStatus
 
-botsettings = BotFrameworkAdapterSettings(app_registration_id, app_registration_secret)
-botadapter = BotFrameworkAdapter(botsettings)
+from aiohttp import web
+from aiohttp.web import Request, Response, json_response
+from botbuilder.core import (
+    BotFrameworkAdapterSettings,
+    TurnContext,
+    BotFrameworkAdapter,
+)
+from botbuilder.core.integration import aiohttp_error_middleware
+from botbuilder.schema import Activity, ActivityTypes
 
-CONMEMORY = ConversationState(MemoryStorage())
-botdialog = QnaBot()
+from bots import QnABot
+from config import DefaultConfig
+
+CONFIG = DefaultConfig()
+
+# Create adapter.
+# See https://aka.ms/about-bot-adapter to learn more about how bots work.
+SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
+ADAPTER = BotFrameworkAdapter(SETTINGS)
 
 
-@app.route("/api/messages", methods=["POST"])
-def messages():
-    if "application/json" in request.headers["content-type"]:
-        body = request.json
-        update_user_input_with_bing_search_response(body=body, bing_search_response=get_bing_search_response(body=body))
-    else:
-        return Response(status=415)
-    activity = Activity().deserialize(body)
-    auth_header = (request.headers["Authorization"] if "Authorization" in request.headers else "")
+# Catch-all for errors.
+async def on_error(context: TurnContext, error: Exception):
+    # This check writes out errors to console log .vs. app insights.
+    # NOTE: In production environment, you should consider logging this to Azure
+    #       application insights.
+    print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
+    traceback.print_exc()
 
-    async def call_fun(turncontext):
-        await botdialog.on_turn(turncontext)
-
-    task = loop.create_task(
-        botadapter.process_activity(activity, auth_header, call_fun)
+    # Send a message to the user
+    await context.send_activity("The bot encountered an error or bug.")
+    await context.send_activity(
+        "To continue to run this bot, please fix the bot source code."
     )
-    loop.run_until_complete(task)
-    return 'OK'
+    # Send a trace activity if we're talking to the Bot Framework Emulator
+    if context.activity.channel_id == "emulator":
+        # Create a trace activity that contains the error object
+        trace_activity = Activity(
+            label="TurnError",
+            name="on_turn_error Trace",
+            timestamp=datetime.utcnow(),
+            type=ActivityTypes.trace,
+            value=f"{error}",
+            value_type="https://www.botframework.com/schemas/error",
+        )
+        # Send a trace activity, which will be displayed in Bot Framework Emulator
+        await context.send_activity(trace_activity)
 
 
-def update_user_input_with_bing_search_response(body, bing_search_response):
+ADAPTER.on_turn_error = on_error
+
+# Create the Bot
+BOT = QnABot(CONFIG)
+
+
+# Listen for incoming requests on /api/messages
+async def messages(req: Request) -> Response:
+    # Main bot message handler.
+    if "application/json" in req.headers["Content-Type"]:
+        body = await req.json()
+    else:
+        return Response(status=HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
+
+    activity = Activity().deserialize(body)
+    auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
+
+    response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
+    if response:
+        return json_response(data=response.body, status=response.status)
+    return Response(status=HTTPStatus.OK)
+    
+def html_response(document):
+    s = open(document, "r")
+    return web.Response(text=s.read(), content_type='text/html')
+
+async def test(req: Request) -> Response:
+    return json_response(data="Hello world", status=HTTPStatus.OK)
+
+async def index(req: Request) -> Response:
+    return html_response('index.html')
+
+def init_func(argv):
+    APP = web.Application(middlewares=[aiohttp_error_middleware])
+    APP.router.add_post("/api/messages", messages)
+    APP.router.add_get("/", index)
+    APP.router.add_get("/chat", index)
+    return APP
+    
+if __name__ == "__main__":
+    APP = init_func(None)
     try:
-        if bing_search_response is not None:
-            body['text'] = bing_search_response
-    except Exception as e:
-        print()
-
-
-def get_bing_search_response(body):
-    try:
-        text = body['text']
-        headers = {"Ocp-Apim-Subscription-Key": bing_search_key}
-        params = {"q": text, "textDecorations": True, "textFormat": "HTML"}
-
-        response = requests.get(bing_search_endpoint, headers=headers, params=params)
-        search_results = response.json()
-        bing_response = search_results['queryContext']['alteredQuery']
-        print('Input from user: {}'.format(text))
-        print('Bing search response: {}'.format(bing_response))
-
-        return bing_response
-    except Exception as e:
-        print(e)
-        return None
-
-
-if __name__ == '__main__':
-    print('Running app')
-    app.run("localhost", 3978)
+        web.run_app(APP, host="0.0.0.0", port=CONFIG.PORT)
+    except Exception as error:
+        raise error
---
+++ bots/qna_bot.py
@@ -0,0 +1,101 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from botbuilder.ai.qna import QnAMaker, QnAMakerEndpoint
+from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
+from botbuilder.schema import ChannelAccount, Attachment
+
+from config import DefaultConfig
+import urllib.parse
+import urllib.request
+import base64
+import json
+import bots.process_image as process_image
+from io import BytesIO
+import requests
+
+
+class QnABot(ActivityHandler):
+    def __init__(self, config: DefaultConfig):
+        self.qna_maker = QnAMaker(
+            QnAMakerEndpoint(
+                knowledge_base_id=config.QNA_KNOWLEDGEBASE_ID,
+                endpoint_key=config.QNA_ENDPOINT_KEY,
+                host=config.QNA_ENDPOINT_HOST,
+            )
+        )
+
+    async def on_members_added_activity(
+        self, members_added: [ChannelAccount], turn_context: TurnContext
+    ):
+        for member in members_added:
+            if member.id != turn_context.activity.recipient.id:
+                await turn_context.send_activity(
+                    "Hey, welcome to the Covid Chatbot.\n\n"
+                    "How may I help you?"
+                )
+
+    async def on_message_activity(self, turn_context: TurnContext):
+        # Check if there are attachments
+        if (turn_context.activity.attachments and len(turn_context.activity.attachments) > 0):
+            await self._handle_incoming_attachment(turn_context)
+            
+        # Split the questions based on ';'
+        questions_array = await self.split_and_separate_questions(turn_context)
+        for question in questions_array:
+            # Bing correction method
+            if question.strip()=='':
+                continue
+            turn_context.activity.text = question
+            await self._get_and_update_bing_search_response(turn_context)
+            # The actual call to the QnA Maker service.
+            response = await self.qna_maker.get_answers(turn_context)
+            if response and len(response) > 0:
+                input_type = "Showing result for -> " if question == turn_context.activity.text else "Did you mean -> "
+                await turn_context.send_activity(input_type + turn_context.activity.text + "\n\n Result -> " + str(MessageFactory.text(response[0].answer).text))
+            else:
+                await turn_context.send_activity("I did not understand the question")
+
+    async def split_and_separate_questions(self, turn_context):
+        text = turn_context.activity.text
+        text_array = text.split(';')
+        return text_array
+
+    async def _get_and_update_bing_search_response(self, turn_context):
+        try:
+            config = DefaultConfig()
+            bing_response = turn_context.activity.text
+            
+            headers = {"Ocp-Apim-Subscription-Key": config.BING_KEY1}
+            params = {"q": bing_response, "textDecorations": True, "textFormat": "HTML"}
+            response = requests.get(config.BING_END_POINT, headers=headers, params=params)
+            search_results = response.json()
+            if 'alteredQuery' in search_results['queryContext']:
+                bing_response = search_results['queryContext']['alteredQuery']
+            if bing_response is not None:
+                turn_context.activity.text = bing_response
+        except Exception as e:
+            print(e)
+            return None
+
+    async def _handle_incoming_attachment(self, turn_context: TurnContext):
+        # Handle attachments uploaded by users. The bot receives an Attachment in an Activity.
+        for attachment in turn_context.activity.attachments:
+            return await self._download_attachment_and_process(attachment, turn_context)
+            
+    async def _download_attachment_and_process(self, attachment: Attachment, turn_context : TurnContext) -> dict:
+        # Retrieve the attachment via the attachment's contentUrl.
+        try:
+            response = urllib.request.urlopen(attachment.content_url)
+            headers = response.info()
+            # If user uploads JSON file, this prevents it from being written as
+            # "{"type":"Buffer","data":[123,13,10,32,32,34,108..."
+            if headers["content-type"] == "application/json":
+                data = bytes(json.load(response)["data"])
+            else:
+                data = response.read()
+            output = process_image.process_image_content(turn_context, BytesIO(data))
+            await output
+        except Exception as exception:
+            print(exception)
+            return {}
