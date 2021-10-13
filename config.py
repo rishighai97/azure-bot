42dbd1a5471e8b09cb93327fb883be85b452e5d3
rishighai97 <rishighai97@gmail.com>
2021-10-13 11:59:34 +0530

    trying to fix images in teams

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ import os
 
 class DefaultConfig:
     """ Bot Configuration """
-
 #    PORT = 3978
 #     APP_ID = os.environ.get("MicrosoftAppId", "")
 #     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
